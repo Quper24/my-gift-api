badd37c110e7ac6c6603db50e632a69c4f5d7331
Maksim Leskin <maksim.leskin@methed.ru>
2023-12-08 02:56:51 +0300

    update

--- index.js
+++ index.js
@@ -1,67 +1,88 @@
 import express from 'express';
 import fs from 'node:fs/promises';
+import crypto from 'crypto';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
 const filePath = './gifts.json';
 
+app.use(cors());
+
+// Парсер для JSON-данных
+app.use(express.json());
+
 // Встроенный парсер для данных формы
 app.use(express.urlencoded({ extended: true }));
 
-/**
- * Сохраняет данные в JSON файл.
- * @param {Object} data - Данные для сохранения.
- */
-async function saveData(data) {
-  try {
-    const jsonData = JSON.stringify(data, null, 2);
-    await fs.writeFile(filePath, jsonData);
-  } catch (error) {
-    console.error('Ошибка при сохранении данных:', error);
-  }
-}
+const generateUniqueId = (length = 10) =>
+  crypto.randomBytes(length).toString('hex').slice(0, length);
 
-/**
- * Загружает данные из JSON файла.
- * @returns {Promise<Object>} - Объект с данными.
- */
-async function loadData() {
+// Асинхронная функция для сохранения данных
+const saveData = async data => {
+  const jsonData = JSON.stringify(data, null, 2);
+  await fs.writeFile(filePath, jsonData);
+};
+
+// Асинхронная функция для загрузки данных
+const loadData = async () => {
   try {
-    await fs.access(filePath); // Проверяем существование файла
+    await fs.access(filePath);
     const data = await fs.readFile(filePath, 'utf8');
     return JSON.parse(data);
   } catch (error) {
     if (error.code === 'ENOENT') {
-      // Если файла нет, создаем пустой объект
       await saveData({});
       return {};
     } else {
-      console.error('Ошибка при чтении данных:', error);
-      return {};
+      throw error;
     }
   }
-}
+};
 
 // Обработка POST-запроса
-app.post('/api/gift', async (req, res) => {
-  const id = Date.now(); // Простой способ сгенерировать уникальный ID
-  const gifts = await loadData();
-  gifts[id] = req.body;
+app.post('/api/gift', async (req, res, next) => {
+  try {
+    console.log(req.body);
+    if (!req.body || Object.keys(req.body).length === 0) {
+      return res.status(400).send({ message: 'Тело запроса отсутствует' });
+    }
 
-  await saveData(gifts);
-  res.send({ message: 'Подарок успешно зарегистрирован', id });
+    const id = generateUniqueId();
+    const gifts = await loadData();
+    gifts[id] = req.body;
+
+    await saveData(gifts);
+    res.send({ message: 'Подарок успешно зарегистрирован', id });
+  } catch (error) {
+    next(error); // Передаем ошибку следующему обработчику
+  }
 });
 
 // Обработка GET-запроса
-app.get('/api/gift/:id', async (req, res) => {
-  const gifts = await loadData();
-  const gift = gifts[req.params.id];
+app.get('/api/gift/:id', async (req, res, next) => {
+  try {
+    const gifts = await loadData();
+    const gift = gifts[req.params.id];
 
-  if (gift) {
-    res.send(gift);
-  } else {
-    res.status(404).send({ message: 'Подарок не найден' });
+    if (gift) {
+      res.send(gift);
+    } else {
+      res.status(404).send({ message: 'Подарок не найден' });
+    }
+  } catch (error) {
+    next(error); // Передаем ошибку следующему обработчику
   }
+});
+
+app.use((req, res, next) => {
+  res.status(404).send({ message: 'Маршрут не найден' });
+});
+
+// Промежуточное ПО для обработки ошибок
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Что-то сломалось!');
 });
 
 app.listen(port, () => {
